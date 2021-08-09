ca0309ce5d7d058967c91763d691929ef9bcd81e
tullii <tullii@hanmail.net>
2021-08-09 20:49:20 +0900

    비밀정보보호

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require('./models/User');
 
-const mongoose = require('mongoose')
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://tullii:ibk1234@cluster0.gw8ch.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(()=> console.log('MongoDB Connected...'))
   .catch(err => console.log)
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! 할 수 있다!')
+});
+
+app.post('/register',(req,res)=>{
+// 회원가입시 필요한 정보를 클라이언트에서 가져와서 디비에 넣어준다
+
+const user = new User(req.body)
+
+user.save((err,userInfo)=>{
+    if(err) return res.json({success:false, err})
+     return res.status(200).json({
+         success:true
+     })
+   })
 })
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-})
+});
